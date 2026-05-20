4d31134861ea215813cfbd6712efccdf76426f5c
khosresw <khosre.swati@gmail.com>
2026-05-20 17:04:01 +0530

    Add box shadow styling to pixelArt element

---
+++ script.js
@@ -0,0 +1,32 @@
+const pixelArt = document.getElementById("pixelArt");
+
+pixelArt.style.boxShadow = `
+20px 0 #000,
+40px 0 #000,
+60px 0 #000,
+80px 0 #000,
+
+0 20px #000,
+20px 20px #fff,
+40px 20px #fff,
+60px 20px #fff,
+80px 20px #000,
+
+0 40px #000,
+20px 40px #fff,
+40px 40px #000,
+60px 40px #fff,
+80px 40px #000,
+
+0 60px #000,
+20px 60px #fff,
+40px 60px #fff,
+60px 60px #fff,
+80px 60px #000,
+
+0 80px #000,
+20px 80px #000,
+40px 80px #000,
+60px 80px #000,
+80px 80px #000
+`;
